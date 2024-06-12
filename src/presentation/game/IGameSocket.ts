bd6a4f25d174b714252c66361e604492323daaf9
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-06-12 22:02:05 +0800

    add: socket io and game socket

---
+++ src/presentation/game/IGameSocket.ts
@@ -0,0 +1,6 @@
+export interface IGameSocket {
+  findGame(): any;
+  move(): any;
+  offerDraw(): any;
+  giveUp(): any;
+}
---
+++ src/presentation/game/game.socket.ts
@@ -0,0 +1,34 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import * as process from 'node:process';
+import { Server } from 'socket.io';
+import { IGameSocket } from './IGameSocket';
+
+@WebSocketGateway({ cors: process.env.CLIENT_URL })
+export class GameSocket implements IGameSocket {
+  @WebSocketServer()
+  socket: Server;
+
+  @SubscribeMessage('find_game')
+  findGame(): any {
+    return 'find_game';
+  }
+
+  @SubscribeMessage('give_up')
+  giveUp(): any {
+    return 'give_up';
+  }
+
+  @SubscribeMessage('move')
+  move(): any {
+    this.socket.emit('move', { message: 'move' });
+  }
+
+  @SubscribeMessage('offer_draw')
+  offerDraw(): any {
+    return 'offer_draw';
+  }
+}

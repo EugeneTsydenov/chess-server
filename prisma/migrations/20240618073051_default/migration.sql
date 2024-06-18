c484b655b8cfdffb7d48de029ae39a4f98959d32
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-06-18 15:43:15 +0800

    _

---
+++ prisma/migrations/20240618073051_default/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "display_name" TEXT NOT NULL,
+    "games" INTEGER NOT NULL DEFAULT 0,
+    "avatar" TEXT NOT NULL DEFAULT 'https://i.ibb.co/LR40jQH/def-avatar.png',
+    "rating" INTEGER NOT NULL DEFAULT 0,
+    "wins" INTEGER NOT NULL DEFAULT 0,
+    "losses" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "draws" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "games" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMPTZ(6) NOT NULL,
+    "white_player" INTEGER NOT NULL,
+    "black_player" INTEGER NOT NULL,
+    "winner" TEXT NOT NULL,
+    "loser" TEXT NOT NULL,
+    "moves" TEXT[],
+
+    CONSTRAINT "games_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tokens" (
+    "id" SERIAL NOT NULL,
+    "jti" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "invalid_tokens" (
+    "id" SERIAL NOT NULL,
+    "jti" TEXT NOT NULL,
+
+    CONSTRAINT "invalid_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tokens_user_id_key" ON "tokens"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
